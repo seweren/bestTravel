8d30e354be4011967cc15d0f6889b92936bf7eea
seweren <seweren@gmail.com>
2019-02-05 23:31:13 +0100

    solution

--- src/bestTravel.ts
+++ src/bestTravel.ts
@@ -1,8 +1,16 @@
 export class BestTravel {
 
-    public static chooseBestSum = (_targetDistance: number,
-        _cityNbrToVisit: number, _list: number[]): number | null => {
-        return null;
+    public static chooseBestSum = (maxDistance: number, cityNbrToVisit: number, cities: number[]): number | null => {
+        const combinationList = combinations(cities.length, cityNbrToVisit);
+        let maxSumOfDistances: number = 0;
+        combinationList.forEach(comb => {
+            const sumOfDistances = comb.map(cityIdx => cities[cityIdx])
+                .reduce<number>((prev, curr) => prev + curr, 0);
+            if (sumOfDistances > maxSumOfDistances && sumOfDistances <= maxDistance) {
+                maxSumOfDistances = sumOfDistances;
+            }
+        });
+        return maxSumOfDistances === 0 ? null : maxSumOfDistances;
     }
 
 }
@@ -11,29 +19,29 @@ export const variations = (n: number, k: number, a: number[][]): number[][] => {
     if (k === 0) {
         return a;
     }
-    let newArray: number[][] = [];
+    const newArray: number[][] = [];
     for (let idxN = 0; idxN < n; idxN++) {
         if (a.length === 0) {
             newArray.push([idxN]);
         } else {
             for (let setIdx = 0; setIdx < a.length; setIdx++) {
-                newArray.push(a[setIdx].concat(idxN).sort());
+                newArray.push(a[setIdx].concat(idxN));
             }
         }
     }
     return variations(n, k - 1, newArray);
-}
+};
 
-const hasNotDuplicateItems = (array: number[], k: number): boolean =>
+const variationHasNotDuplicateItems = (array: number[], k: number): boolean =>
     new Set<number>(array).size === k;
 
+const hasVariationWithSameItems = (variationList: number[][], variation: number[]): boolean =>
+    variationList.some(v => JSON.stringify(v.sort()) === JSON.stringify(variation.sort()));
+
 export const combinations = (n: number, k: number): number[][] => {
-    let vars = variations(n, k, []);
+    const vars = variations(n, k, []);
     return vars
-        .filter(variation => hasNotDuplicateItems(variation, k))
-        .reduce<number[][]>((resultsVars, variation) =>
-            resultsVars.some(v => JSON.stringify(v) === JSON.stringify(variation))
-                ? resultsVars
-                : resultsVars.concat([variation])
-            , []);
+        .filter(variation => variationHasNotDuplicateItems(variation, k))
+        .reduce<number[][]>((resultsVars, variation) => hasVariationWithSameItems(resultsVars, variation)
+            ? resultsVars : resultsVars.concat([variation]), []);
 };
--- test/tests.ts
+++ test/tests.ts
@@ -1,6 +1,6 @@
 import { deepStrictEqual, equal } from "assert";
 
-import { BestTravel, combinations } from "../src/bestTravel";
+import { BestTravel, combinations, variations } from "../src/bestTravel";
 
 /// <reference types="mocha" />
 function dotest(t: number, k: number, ls: number[], expected: number | null) {
@@ -9,31 +9,46 @@ function dotest(t: number, k: number, ls: number[], expected: number | null) {
 
 describe("Fixed Tests", () => {
 
+    describe("variations Tests", () => {
+
+        it("variations 3 2", () => {
+            deepStrictEqual(variations(3, 2, []), [
+                [0, 0], [1, 0], [2, 0],
+                [0, 1], [1, 1], [2, 1],
+                [0, 2], [1, 2], [2, 2],
+            ]);
+        });
+
+    });
+
     describe("combinations Tests", () => {
 
         it("combinations 3 2", () => {
-            deepStrictEqual(combinations(3, 2), [[0, 1], [0, 2], [1, 2]]);
+            deepStrictEqual(combinations(3, 2), [
+                [0, 1], [0, 2], [1, 2],
+            ]);
         });
         it("combinations 4 2", () => {
-            deepStrictEqual(combinations(4, 2),
-                [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]);
+            deepStrictEqual(combinations(4, 2), [
+                [0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3],
+            ]);
         });
 
     });
 
     describe("chooseBestSum Tests", () => {
 
-        it.skip("choosebestsum 1", () => {
+        it("choosebestsum 1", () => {
             const ts = [50, 55, 56, 57, 58];
             dotest(163, 3, ts, 163);
         });
 
-        it.skip("choosebestsum 2", () => {
+        it("choosebestsum 2", () => {
             const ts = [50];
             dotest(163, 3, ts, null);
         });
 
-        it.skip("choosebestsum 3", () => {
+        it("choosebestsum 3", () => {
             const ts = [91, 74, 73, 85, 73, 81, 87];
             dotest(230, 3, ts, 228);
         });
